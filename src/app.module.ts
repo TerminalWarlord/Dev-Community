d0a4a5fff4d49c8c520811323b80608c0d43dd28
terminalwarlord <joybiswas389@gmail.com>
2026-05-13 15:36:23 +0600

    feat: add mongodb connection using MongooseModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,19 @@
 import { Module } from '@nestjs/common';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import "dotenv/config";
+const DATABASE_URL = process.env.DATABASE_URL;
+if (!DATABASE_URL) {
+  console.error("DATABASE_URL variable is not set!")
+  process.exit(1);
+}
 
 @Module({
-  imports: [UserModule, AuthModule],
+  imports: [
+    UserModule,
+    AuthModule,
+    MongooseModule.forRoot(DATABASE_URL)
+  ],
 })
 export class AppModule { }
