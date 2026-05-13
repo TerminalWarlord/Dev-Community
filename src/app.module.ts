cbc4ba4a460b94e69a9d400210202d1d39331a6c
terminalwarlord <joybiswas389@gmail.com>
2026-05-13 11:49:29 +0600

    feat: add user module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
+import { UsersModule } from './user/users.module';
 
 @Module({
-  imports: [],
+  imports: [UsersModule],
 })
 export class AppModule { }
---
+++ src/user/users.controller.ts
@@ -0,0 +1,8 @@
+import { Controller } from '@nestjs/common';
+import { UsersService } from './users.service';
+
+@Controller('user')
+export class UsersController {
+    constructor(private userService: UsersService) {
+    }
+}
