72ee517bb39542884a8232814093051709c404bb
terminalwarlord <joybiswas389@gmail.com>
2026-05-14 15:04:45 +0600

    feat: add user endpoints

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,8 +1,42 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Delete, Patch, Post } from '@nestjs/common';
 import { UserService } from './user.service';
 
 @Controller('user')
 export class UserController {
     constructor(private userService: UserService) {
+    }
+
+    @Patch("change-password")
+    async changePassword() {
+        return this.userService.changePassword();
+    }
+    // TODO: add new module for skill, experience, invitation
+    @Post("skill/add")
+    async addSkill() {
+        return this.userService.addSkill();
+    }
+    @Delete("skill/delete")
+    async removeSkill() {
+        return this.userService.removeSkill();
+    }
+    @Post("experience/add")
+    async addExperience() {
+        return this.userService.addExperience();
+    }
+    @Patch("experience/update")
+    async updateExperience() {
+        return this.userService.updateExperience();
+    }
+    @Delete("experience/delete")
+    async removeExperience() {
+        return this.userService.removeExperience();
+    }
+    @Post("invitation/accept/:invitationId")
+    async acceptInvitation() {
+        return this.userService.acceptInvitation();
+    }
+    @Post("invitation/reject/:invitationId")
+    async rejectInvitation() {
+        return this.userService.rejectInvitation();
     }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -2,4 +2,20 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class UserService {
+    async changePassword() {
+    }
+    async addSkill() {
+    }
+    async removeSkill() {
+    }
+    async addExperience() {
+    }
+    async updateExperience() {
+    }
+    async removeExperience() {
+    }
+    async acceptInvitation() {
+    }
+    async rejectInvitation() {
+    }
 }
