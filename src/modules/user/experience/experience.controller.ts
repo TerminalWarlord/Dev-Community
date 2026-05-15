276fb5ecaa3d0916e1d329cf9e185cc21542423b
terminalwarlord <joybiswas389@gmail.com>
2026-05-15 16:45:39 +0600

    feat: add logic to delete and update experience

--- src/modules/user/experience/experience.controller.ts
+++ src/modules/user/experience/experience.controller.ts
@@ -2,29 +2,37 @@ import { Body, Controller, Delete, Get, Patch, Post, Request, UseGuards } from '
 import { ExperienceService } from './experience.service';
 import { CreateExperienceDto } from './dto/create-experience.dto';
 import { AuthGuard } from 'src/modules/auth/auth.guard';
+import { RemoveExperienceDto } from './dto/remove-experience.dto';
+import { UpdateExperienceDto } from './dto/update-experience.dto';
 
 
+@UseGuards(AuthGuard)
 @Controller('user/experience')
 export class ExperienceController {
   constructor(
     private experienceService: ExperienceService
   ) { }
 
-  @UseGuards(AuthGuard)
   @Post('add')
   async addExperience(
     @Body() createExperienceDto: CreateExperienceDto,
-    @Request() req: {userId: string}
+    @Request() req: { userId: string }
   ) {
-    return this.experienceService.addExperience({...createExperienceDto, userId: req.userId});
+    return this.experienceService.addExperience({ ...createExperienceDto, userId: req.userId });
   }
   @Patch('update')
-  async updateExperience() {
-    return this.experienceService.updateExperience();
+  async updateExperience(
+    @Body() updateExperienceDto: UpdateExperienceDto,
+    @Request() req: { userId: string }
+  ) {
+    return this.experienceService.updateExperience({ ...updateExperienceDto, userId: req.userId });
   }
   @Delete('delete')
-  async removeExperience() {
-    return this.experienceService.removeExperience();
+  async removeExperience(
+    @Body() removeExperienceDto: RemoveExperienceDto,
+    @Request() req: { userId: string }
+  ) {
+    return this.experienceService.removeExperience({ ...removeExperienceDto, userId: req.userId });
   }
 
 }
--- src/modules/user/experience/experience.service.ts
+++ src/modules/user/experience/experience.service.ts
@@ -1,8 +1,10 @@
 import { Injectable, InternalServerErrorException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
-import { Model } from 'mongoose';
+import mongoose, { Model } from 'mongoose';
 import { Experience } from 'src/schemas/experience.schema';
 import { CreateExperienceDto } from './dto/create-experience.dto';
+import { RemoveExperienceDto } from './dto/remove-experience.dto';
+import { UpdateExperienceDto } from './dto/update-experience.dto';
 
 @Injectable()
 export class ExperienceService {
@@ -30,10 +32,37 @@ export class ExperienceService {
     }
   }
 
-  async updateExperience() {
-    return {}
+  async updateExperience(updateExperienceDto: UpdateExperienceDto) {
+    try {
+      await this.experienceModel.updateOne({
+        _id: updateExperienceDto.experienceId,
+        userId: new mongoose.Types.ObjectId(updateExperienceDto.experienceId),
+      }, {
+        ...updateExperienceDto
+      })
+      return {
+        message: "success"
+      }
+
+    } catch (error) {
+      throw new InternalServerErrorException("Failed to update experience");
+    }
   }
-  async removeExperience() {
-    return {}
+  async removeExperience(removeExperienceDto: RemoveExperienceDto) {
+    try {
+      const experience = await this.experienceModel.findOneAndDelete({
+        userId: new mongoose.Types.ObjectId(removeExperienceDto.userId),
+        _id: new mongoose.Types.ObjectId(removeExperienceDto.experienceId)
+      })
+      if (!experience) {
+        throw new InternalServerErrorException("Failed to delete experience");
+      }
+      return {
+        message: "success"
+      }
+    }
+    catch (err) {
+      throw new InternalServerErrorException("Failed to delete experience");
+    }
   }
 }
