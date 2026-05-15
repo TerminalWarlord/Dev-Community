844aa470d29e5580fff9e6d230257a37cac3a64e
terminalwarlord <joybiswas389@gmail.com>
2026-05-15 16:34:32 +0600

    feat: add logic to create experience

--- src/modules/user/experience/experience.controller.ts
+++ src/modules/user/experience/experience.controller.ts
@@ -1,5 +1,7 @@
-import { Controller, Delete, Get, Patch, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Patch, Post, Request, UseGuards } from '@nestjs/common';
 import { ExperienceService } from './experience.service';
+import { CreateExperienceDto } from './dto/create-experience.dto';
+import { AuthGuard } from 'src/modules/auth/auth.guard';
 
 
 @Controller('user/experience')
@@ -8,9 +10,13 @@ export class ExperienceController {
     private experienceService: ExperienceService
   ) { }
 
+  @UseGuards(AuthGuard)
   @Post('add')
-  async addExperience() {
-    return this.experienceService.addExperience();
+  async addExperience(
+    @Body() createExperienceDto: CreateExperienceDto,
+    @Request() req: {userId: string}
+  ) {
+    return this.experienceService.addExperience({...createExperienceDto, userId: req.userId});
   }
   @Patch('update')
   async updateExperience() {
--- src/modules/user/experience/experience.service.ts
+++ src/modules/user/experience/experience.service.ts
@@ -1,7 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, InternalServerErrorException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { Experience } from 'src/schemas/experience.schema';
+import { CreateExperienceDto } from './dto/create-experience.dto';
 
 @Injectable()
 export class ExperienceService {
@@ -10,8 +11,23 @@ export class ExperienceService {
     private readonly experienceModel: Model<Experience>,
   ) { }
 
-  async addExperience() {
-    return {}
+  // TODO: check duplicates
+  async addExperience(createExperienceDto: CreateExperienceDto) {
+    try {
+      const experience = await this.experienceModel.insertOne({
+        ...createExperienceDto,
+        startDate: new Date(createExperienceDto.startDate),
+        endDate: new Date(createExperienceDto.endDate),
+      })
+      return {
+        message: "success",
+        experienceId: experience._id
+      };
+
+    } catch (error) {
+      console.log(error)
+      throw new InternalServerErrorException("Failed to create experience");
+    }
   }
 
   async updateExperience() {
