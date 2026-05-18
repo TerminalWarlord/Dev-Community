31109637330ba72a051916de450c06db08e023c9
terminalwarlord <joybiswas389@gmail.com>
2026-05-18 09:30:26 +0600

    feat: add class-validator in experience DTOs

--- src/modules/user/experience/dto/update-experience.dto.ts
+++ src/modules/user/experience/dto/update-experience.dto.ts
@@ -1,8 +1,21 @@
+import { IsDate, IsString } from "class-validator";
+
 export class UpdateExperienceDto {
+  @IsString()
   experienceId!: string;
+
+  @IsString()
   companyName!: string;
+
+  @IsString()
   userId!: string;
+
+  @IsString()
   experienceTitle!: string;
+
+  @IsDate()
   startDate!: string;
+
+  @IsDate()
   endDate!: string;
 }
