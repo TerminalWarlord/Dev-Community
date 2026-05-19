85daeab6f7587cddc1a662eed9ea38abd6eb6c03
terminalwarlord <joybiswas389@gmail.com>
2026-05-19 12:51:45 +0600

    feat: add Post schema

---
+++ src/schemas/post.schema.ts
@@ -0,0 +1,33 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import mongoose, { HydratedDocument } from 'mongoose';
+import { PostStatus } from 'src/common/post.enum';
+
+export type PostDocument = HydratedDocument<Post>;
+
+@Schema({ timestamps: true })
+export class Post {
+  @Prop({ type: String, required: true })
+  title!: string;
+
+  @Prop({ type: String, required: true })
+  slug!: string;
+
+  @Prop({ type: String, required: true })
+  content!: string;
+
+  @Prop({ type: mongoose.Types.ObjectId, required: true, ref: "User" })
+  postedBy!: mongoose.Types.ObjectId;
+
+  @Prop({ type: mongoose.Types.ObjectId, ref: "Community" })
+  communityId!: mongoose.Types.ObjectId;
+
+  @Prop({ type: String, enum: PostStatus, default: PostStatus.PUBLISHED })
+  status!: mongoose.Types.ObjectId;
+}
+
+export const SkillSchema = SchemaFactory.createForClass(Post);
+
+SkillSchema.index({
+  communityId: 1,
+  slug: 1
+}, { unique: true });
