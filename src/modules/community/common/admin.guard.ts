597b7068f5a4d7e1296571e4b5a7bc1bf7e39bb2
terminalwarlord <joybiswas389@gmail.com>
2026-05-19 09:53:56 +0600

    feat: add guards for admin and moderator

---
+++ src/modules/community/common/admin.guard.ts
@@ -0,0 +1,60 @@
+import {
+  CanActivate,
+  ExecutionContext,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { Request } from 'express';
+import { JWT_SECRET } from '../../../common/constants';
+import { InjectModel } from '@nestjs/mongoose';
+import { User } from 'src/schemas/user.schema';
+import mongoose, { Model } from 'mongoose';
+import { CommunityRole } from 'src/schemas/community-role.schema';
+
+@Injectable()
+export class CommunityAdminAuthGuard implements CanActivate {
+  constructor(
+    @InjectModel(User.name)
+    private readonly communityRoleModel: Model<CommunityRole>,
+    private readonly jwtService: JwtService,
+  ) { }
+
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const request = context.switchToHttp().getRequest();
+    const token = this.extractTokenFromHeader(request);
+    if (!token) {
+      throw new UnauthorizedException();
+    }
+    try {
+      const payload = await this.jwtService.verifyAsync(token, {
+        secret: JWT_SECRET,
+      });
+      if (!payload.userId) {
+        throw new UnauthorizedException();
+      }
+      // extract params
+      const communityId = request.params.communityId;
+      if (!communityId) {
+        throw new UnauthorizedException();
+      }
+      // check if userId
+      const communityRole = await this.communityRoleModel.findOne({
+        userId: new mongoose.Types.ObjectId(payload.userId),
+        communityId: new mongoose.Types.ObjectId(communityId),
+      });
+      if (!communityRole || communityRole.role !== "ADMIN") {
+        throw new UnauthorizedException("You are not an admin!");
+      }
+    } catch (err) {
+      console.log(err);
+      throw new UnauthorizedException();
+    }
+    return true;
+  }
+
+  private extractTokenFromHeader(request: Request): string | undefined {
+    const [type, token] = request.headers.authorization?.split(' ') ?? [];
+    return type === 'Bearer' ? token : undefined;
+  }
+}
