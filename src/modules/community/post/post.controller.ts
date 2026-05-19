ee28554d7c902b23df301b6f3dc912e793ec15cf
terminalwarlord <joybiswas389@gmail.com>
2026-05-19 12:37:23 +0600

    feat: add post endpoints

--- src/modules/community/post/post.controller.ts
+++ src/modules/community/post/post.controller.ts
@@ -1,4 +1,40 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
+import { PostService } from './post.service';
 
-@Controller('post')
-export class PostController {}
+@Controller('community/:communityId/post')
+export class PostController {
+  constructor(private postService: PostService) { }
+  @Post('create')
+  async createCommunityPost() {
+  }
+
+  @Patch('update')
+  async updateCommunityPost() {
+  }
+
+  @Post('vote')
+  async voteCommunityPost() {
+  }
+
+
+  @Delete('delete')
+  async deleteCommunityPost() {
+  }
+
+
+  @Get(':postSlug/comment/all')
+  async getCommunityPostComments() {
+  }
+
+  @Post(':postSlug/comment/add')
+  async addACommunityPostComment() {
+  }
+
+  @Patch(':postSlug/comment/update')
+  async updateACommunityPostComment() {
+  }
+
+  @Delete(':postSlug/comment/update')
+  async deleteACommunityPostComment() {
+  }
+}
