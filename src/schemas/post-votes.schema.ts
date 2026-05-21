b33ca723b38b6640def7ea7ab9a128ca8419a154
terminalwarlord <joybiswas389@gmail.com>
2026-05-21 09:20:43 +0600

    refactor: update PostVote schema exports

--- src/schemas/post-votes.schema.ts
+++ src/schemas/post-votes.schema.ts
@@ -2,7 +2,7 @@ import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import mongoose, { HydratedDocument } from 'mongoose';
 import { VoteType } from 'src/common/post.enum';
 
-export type ExperienceDocument = HydratedDocument<PostVote>;
+export type PostVoteDocument = HydratedDocument<PostVote>;
 
 @Schema({ timestamps: true })
 export class PostVote {
