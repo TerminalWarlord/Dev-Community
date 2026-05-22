8af0d6ea66adfe284af35df3fb115952665ef47c
terminalwarlord <joybiswas389@gmail.com>
2026-05-22 15:04:08 +0600

    feat: add bullmq to the project

--- src/modules/post/post.module.ts
+++ src/modules/post/post.module.ts
@@ -8,10 +8,14 @@ import { User, UserSchema } from 'src/schemas/user.schema';
 import { CommunityRole, CommunityRoleSchema } from 'src/schemas/community-role.schema';
 import { PostVote, PostVoteSchema } from 'src/schemas/post-votes.schema';
 import { CommentModule } from '../comment/comment.module';
+import { BullModule } from '@nestjs/bullmq';
 
 @Module({
   imports: [
     JwtModule,
+    BullModule.registerQueue({
+      name: 'posts'
+    }),
     MongooseModule.forFeature([
       { name: Post.name, schema: PostSchema },
       { name: User.name, schema: UserSchema },
