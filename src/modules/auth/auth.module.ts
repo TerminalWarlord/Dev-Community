4ccd0e9f9305b1062181b2a3c6cd14b7d7dc70ba
terminalwarlord <joybiswas389@gmail.com>
2026-06-02 14:43:53 +0600

    refactor: db logics in auth services

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -2,17 +2,19 @@ import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { UserModule } from 'src/modules/user/user.module';
-import { MongooseModule } from '@nestjs/mongoose';
-import { User, UserSchema } from 'src/schemas/user.schema';
 import { JwtModule } from '@nestjs/jwt';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from '../../entities/user.entity';
 
 @Module({
   imports: [
     JwtModule.register({}),
-    MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+    TypeOrmModule.forFeature([
+      User
+    ]),
     UserModule,
   ],
   controllers: [AuthController],
   providers: [AuthService],
 })
-export class AuthModule {}
+export class AuthModule { }
