061ddfd5ceec188c38288f78b6417888a8fe8f28
terminalwarlord <joybiswas389@gmail.com>
2026-06-03 12:34:04 +0600

    refactor: update db logics in the community optional guard

--- src/modules/community/common/optional-read.guard.ts
+++ src/modules/community/common/optional-read.guard.ts
@@ -1,4 +1,5 @@
 import {
+  BadRequestException,
   CanActivate,
   ExecutionContext,
   Injectable,
@@ -7,22 +8,22 @@ import {
 } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { Request } from 'express';
-import { InjectModel } from '@nestjs/mongoose';
-import mongoose, { Model } from 'mongoose';
-import { CommunityRole } from 'src/schemas/community-role.schema';
 import { MembershipStatus } from 'src/common/community.enum';
 import { CommunityService } from '../community.service';
-import { User } from 'src/schemas/user.schema';
 import { ConfigService } from '@nestjs/config';
+import { InjectRepository } from '@nestjs/typeorm';
+import { CommunityRole } from 'src/entities/community-role.entity';
+import { Repository } from 'typeorm';
+import { User } from 'src/entities/user.entity';
 
 @Injectable()
 export class OptionalReadAccessGuard implements CanActivate {
   private logger = new Logger(CommunityService.name);
   constructor(
-    @InjectModel(CommunityRole.name)
-    private readonly communityRoleModel: Model<CommunityRole>,
-    @InjectModel(User.name)
-    private readonly userModel: Model<User>,
+    @InjectRepository(CommunityRole)
+    private readonly communityRoleRepo: Repository<CommunityRole>,
+    @InjectRepository(User)
+    private readonly userRepo: Repository<User>,
     private readonly jwtService: JwtService, private configService: ConfigService
   ) { }
 
@@ -38,24 +39,40 @@ export class OptionalReadAccessGuard implements CanActivate {
         secret: JWT_SECRET,
       });
       if (payload.userId) {
-        const user = await this.userModel.find({
-          _id: new mongoose.Types.ObjectId(payload.userId)
+        const userId = parseInt(payload.userId);
+        if (!userId || Number.isNaN(userId)) {
+          throw new BadRequestException("Please provide a valid userId")
+        }
+        const user = await this.userRepo.find({
+          where: {
+            id: userId
+          }
         });
         if (!user) {
-          throw new UnauthorizedException();
+          throw new UnauthorizedException("Invalid user");
         }
-        request["userId"] = payload.userId;
+        request["userId"] = parseInt(payload.userId);
       }
 
       // extract communityId from body or queries
       const communityId = request.body?.communityId ?? request.query?.communityId;
       if (communityId) {
+        const parsedCommunityId = parseInt(communityId);
+        if (Number.isNaN(parsedCommunityId)) {
+          throw new BadRequestException("Invalid community!")
+        }
         if (!request["userId"]) {
           throw new UnauthorizedException("You are not a member of this community!");
         }
-        const communityRole = await this.communityRoleModel.findOne({
-          userId: new mongoose.Types.ObjectId(payload.userId),
-          communityId: new mongoose.Types.ObjectId(communityId),
+        const communityRole = await this.communityRoleRepo.findOne({
+          where: {
+            user: {
+              id: parseInt(payload.userId)
+            },
+            community: {
+              id: communityId
+            }
+          }
         });
         if (!communityRole) {
           throw new UnauthorizedException("You are not a member of this community!");
@@ -66,8 +83,8 @@ export class OptionalReadAccessGuard implements CanActivate {
       }
     } catch (err) {
       this.logger.error(err);
-      if (err instanceof Error) {
-        throw new UnauthorizedException(err.message);
+      if (err instanceof BadRequestException) {
+        throw new BadRequestException(err.message);
       }
       throw new UnauthorizedException();
     }
