39b13058e993a6cb4dc4e5749a693945738a9429
terminalwarlord <joybiswas389@gmail.com>
2026-06-04 15:06:39 +0600

    refactor: add time in the date formatting helper

--- src/common/format-date.ts
+++ src/common/format-date.ts
@@ -1,7 +1,14 @@
 export function formatDate(dateStr: string) {
   const date = new Date(dateStr);
   const formattedDate = new Intl.DateTimeFormat("en-US", {
-    timeZone: "Asia/Dhaka"
+    timeZone: "Asia/Dhaka",
+    day: "2-digit",
+    year: "numeric",
+    month: "2-digit",
+    hour: "2-digit",
+    minute: "2-digit",
+    second: "2-digit",
+    hour12: false
   });
   return formattedDate.format(date);
 }
