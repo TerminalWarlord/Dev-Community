09f95414d1eadbfeaf3a883d0d72819f3654ef82
terminalwarlord <joybiswas389@gmail.com>
2026-06-04 09:26:51 +0600

    refactor: return user data in the google callback endpoint

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,11 +1,13 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post, Request, Res, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { CreateUserDto } from '../user/dto/create-user.dto';
 import { LoginUserDto } from 'src/modules/user/dto/login-user.dto';
+import { GoogleAuthGuard } from './utils/google-auth.guard';
+import { GoogleOAuthCallbackDto } from './dto/callback.dto';
 
 @Controller('auth')
 export class AuthController {
-  constructor(private authService: AuthService) {}
+  constructor(private authService: AuthService) { }
   @Post('signup')
   async signUp(@Body() createUserDto: CreateUserDto) {
     return this.authService.signUp(createUserDto);
@@ -14,5 +16,17 @@ export class AuthController {
   @Post('login')
   async logIn(@Body() loginUserDto: LoginUserDto) {
     return this.authService.logIn(loginUserDto);
+  }
+
+  @UseGuards(GoogleAuthGuard)
+  @Get('google/login')
+  async googleLogin() {
+    return {};
+  }
+
+  @Get('google/callback')
+  @UseGuards(GoogleAuthGuard)
+  async googleRedirect(@Request() req: { user: GoogleOAuthCallbackDto }) {
+    return req.user;
   }
 }
