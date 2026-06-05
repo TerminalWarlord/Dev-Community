ef3bd578d546e30de443b3aef22cd9f329876a1e
terminalwarlord <joybiswas389@gmail.com>
2026-06-05 14:31:05 +0600

    refactor: update the db mocks for user tests

--- src/modules/user/user.controller.spec.ts
+++ src/modules/user/user.controller.spec.ts
@@ -1,12 +1,11 @@
 import { Test } from '@nestjs/testing';
-import { getModelToken } from '@nestjs/mongoose';
-import { User } from 'src/schemas/user.schema';
 import { UserService } from './user.service';
-import { Experience } from 'src/schemas/experience.schema';
-import { UserSkill } from 'src/schemas/user-skill.schema';
-import mongoose from 'mongoose';
 import { ForbiddenException, NotFoundException, UnauthorizedException } from '@nestjs/common';
 import bcrypt from "bcrypt";
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { User } from 'src/entities/user.entity';
+import { UserSkill } from 'src/entities/user-skill.entity';
+import { Experience } from 'src/entities/experience.entity';
 
 describe("UserController", () => {
   let service: UserService;
@@ -18,50 +17,37 @@ describe("UserController", () => {
     mockUserModel = {
       updateOne: jest.fn().mockImplementation((data: object) => Promise.resolve(data)),
       findById: jest.fn().mockReturnThis(),
-      select: jest.fn().mockResolvedValue({ userId: new mongoose.Types.ObjectId().toString() })
+      findOne: jest.fn().mockImplementation((data) => Promise.resolve(data)),
+      update: jest.fn()
     }
 
 
     mockUserSkillModel = {
       insertOne: jest.fn().mockImplementation((data: object) => Promise.resolve(data)),
       findOne: jest.fn().mockImplementation(() => Promise.resolve(null)),
-      find: jest.fn().mockReturnThis(),
-      populate: jest.fn().mockReturnThis(),
-      select: jest.fn().mockReturnThis(),
-      skip: jest.fn().mockReturnThis(),
-      limit: jest.fn().mockResolvedValue([
-        {
-          _id: new mongoose.Types.ObjectId().toString(),
-          skillId: { _id: 'skillId123', skillTitle: 'TypeScript' }
-        }
-      ])
+      find: jest.fn().mockResolvedValue([{
+        id: 1,
+        skill: { id: 2, skillTitle: 'TypeScript' }
+      }]),
     }
 
     mockExperienceModel = {
       insertOne: jest.fn().mockImplementation((data: object) => Promise.resolve(data)),
-      find: jest.fn().mockReturnThis(),
-      populate: jest.fn().mockReturnThis(),
-      select: jest.fn().mockReturnThis(),
-      skip: jest.fn().mockReturnThis(),
-      limit: jest.fn().mockResolvedValue([
-        {
-          _id: new mongoose.Types.ObjectId().toString(),
-        }
-      ])
+      find: jest.fn().mockImplementation((data) => Promise.resolve([data])),
     }
     const module = await Test.createTestingModule({
       providers: [
         UserService,
         {
-          provide: getModelToken(User.name),
+          provide: getRepositoryToken(User),
           useValue: mockUserModel,
         },
         {
-          provide: getModelToken(UserSkill.name),
+          provide: getRepositoryToken(UserSkill),
           useValue: mockUserSkillModel,
         },
         {
-          provide: getModelToken(Experience.name),
+          provide: getRepositoryToken(Experience),
           useValue: mockExperienceModel,
         },
       ],
@@ -76,72 +62,62 @@ describe("UserController", () => {
 
 
   it('can get user profile', async () => {
-    const userId = new mongoose.Types.ObjectId().toString();
-    const profile = await service.getUserProfile(userId);
+    const profile = await service.getUserProfile(1);
     expect(profile).toBeDefined();
   });
 
 
   it('throws error if user provides an invalid userId', async () => {
-    mockUserModel.findById = jest.fn().mockReturnValue({
-      select: jest.fn().mockResolvedValue(null)
-    });
-    const userId = new mongoose.Types.ObjectId().toString();
-    await expect(service.getUserProfile(userId)).rejects.toThrow(NotFoundException)
+    mockUserModel.findOne = jest.fn().mockReturnValue(null);
+    await expect(service.getUserProfile(1)).rejects.toThrow(NotFoundException)
   });
 
 
   it("can get user's skills", async () => {
-    const userId = new mongoose.Types.ObjectId().toString();
     const userSkills = await service.getUserSkills(
-      { userId },
-      { page: 1, limit: 10 }
+      { userId: "1" },
+      { page: "1", limit: "10" }
     );
     expect(userSkills).toBeDefined();
   })
 
   it("can get user's experiences", async () => {
-    const userId = new mongoose.Types.ObjectId().toString();
     const userExperiences = await service.getUserExperiences(
-      { userId },
-      { page: 1, limit: 10 }
+      { userId: "1" },
+      { page: "1", limit: "10" }
     );
     expect(userExperiences).toBeDefined();
   })
 
 
   it("allows user to change password", async () => {
-    const userId = new mongoose.Types.ObjectId().toString();
     const oldPassword = "1234";
     const newPassword = "12345";
-    mockUserModel.findById = jest.fn().mockResolvedValue({ password: await bcrypt.hash(oldPassword, 10) });
+    mockUserModel.findOne = jest.fn().mockResolvedValue({ password: await bcrypt.hash(oldPassword, 10) });
     await service.changePassword({
       newPassword,
       oldPassword
-    }, userId);
+    }, { userId: "1" });
   })
 
   it("throws error if user provides wrong password while changing password", async () => {
-    const userId = new mongoose.Types.ObjectId().toString();
     const oldPassword = "12346";
     const newPassword = "12345";
-    mockUserModel.findById = jest.fn().mockResolvedValue({ password: await bcrypt.hash(oldPassword, 10) });
+    mockUserModel.findOne = jest.fn().mockResolvedValue({ password: await bcrypt.hash(oldPassword, 10) });
     await expect(service.changePassword({
       newPassword,
       oldPassword: "1234"
-    }, userId)).rejects.toThrow(ForbiddenException);
+    }, { userId: "1" })).rejects.toThrow(ForbiddenException);
   })
 
 
   it("throws error if user provides wrong userId while changing password", async () => {
-    const userId = new mongoose.Types.ObjectId().toString();
     const oldPassword = "12346";
     const newPassword = "12345";
-    mockUserModel.findById = jest.fn().mockResolvedValue(null);
+    mockUserModel.findOne = jest.fn().mockResolvedValue(null);
     await expect(service.changePassword({
       newPassword,
       oldPassword
-    }, userId)).rejects.toThrow(UnauthorizedException);
+    }, { userId: '1' })).rejects.toThrow(UnauthorizedException);
   })
-
 })
