ec5e83d135db9c0342afbdd113a376a664b47ba7
terminalwarlord <joybiswas389@gmail.com>
2026-06-05 12:07:59 +0600

    chore: remove all mongoose schemas and imports of them

--- src/modules/community/community.module.ts
+++ src/modules/community/community.module.ts
@@ -3,7 +3,6 @@ import { CommunityController } from './community.controller';
 import { CommunityService } from './community.service';
 import { JwtModule } from '@nestjs/jwt';
 import { MongooseModule } from '@nestjs/mongoose';
-import { Comment, CommentSchema } from 'src/schemas/comment.schema';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Community } from 'src/entities/community.entity';
 import { CommunityRole } from 'src/entities/community-role.entity';
@@ -12,9 +11,6 @@ import { User } from 'src/entities/user.entity';
 @Module({
   imports: [
     JwtModule,
-    MongooseModule.forFeature([
-      { name: Comment.name, schema: CommentSchema },
-    ]),
     TypeOrmModule.forFeature([
       User,
       Community,
